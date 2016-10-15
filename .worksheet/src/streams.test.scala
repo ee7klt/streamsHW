3ed77f444b346ff55005915044f8c4875a446164
KL Tah <kltah1@gmail.com>
2016-10-15 12:04:28 +0800

    remove ignored files

--- .worksheet/src/streams.test.scala
+++
@@ -1,9 +0,0 @@
-package streams
-
-
-import streams.Bloxorz._
-
-object test {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(69); 
-  val a = 2;System.out.println("""a  : Int = """ + $show(a ))}
-  
-}
